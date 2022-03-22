59f71c902b05647fe24e36d002314442ddffce05
MrxinL <64063895+MrxinL@users.noreply.github.com>
2022-03-22 18:47:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './permission'
+import Element from 'element-ui'
+import './style/index.less'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from './utils/axios.config'
+import  Conponent from './components/index.js'
+
+
+// axios.defaults.baseURL ='http://localhost:3000'
+Vue.prototype.$axios =axios  //全局使用
+
+Vue.use(Element)
+Vue.use(Conponent)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
---
+++ permission.js
@@ -0,0 +1,26 @@
+import router from './rouder'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+// 导航守卫
+// 路由发生改变之前  打开
+
+router.beforeEach(function(to,from,next){
+  nprogress.start()  //开启进度条
+  if(to.path.startsWith('/home')){
+    const token = window.localStorage.getItem('user-token')
+    if(token){
+      next()
+    }else{
+      next('/login')
+    }
+  }
+  else{
+    next()
+  }
+})
+
+//路由完成后关闭进度条
+router.afterEach(function(){
+  nprogress.done() // 关闭进度条
+})
+export default  router
