ef44fb09fce2bde83a264aa4e7e4a44484bd43c4
MrxinL <64063895+MrxinL@users.noreply.github.com>
2022-03-22 19:05:11 +0800

    Add files via upload

--- src/views/yeji/index.vue
+++ src/views/yeji/index.vue
@@ -1,18 +1,5 @@
 <template>
 <div>
-    <div class="buju">
-       <el-input v-model="postdata.inputname" placeholder="姓名" class="input"></el-input>
-       <el-input v-model="postdata.inputphone" placeholder="电话号" class="input"></el-input>
-          <el-select v-model="postdata.inputvalue" placeholder="请选择">
-            <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-            </el-option>
-        </el-select>
-       <el-button type="primary" @click="hanleadd">添加</el-button>
-    </div>
     <div class="max">
         <span class="itemmax">销售数量: {{length}}</span>
         <span class="itemmax">客单价: {{price}}</span>
@@ -40,11 +27,16 @@
       label="购买课程"
       width="180">
       <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.value }}</span>
+        <span style="margin-left: 10px">{{ scope.row.xueke }}</span>
       </template>
     </el-table-column>
-    
-  
+        <el-table-column
+      label="所属班级"
+      width="180">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.banji }}</span>
+      </template>
+    </el-table-column>
     <el-table-column label="操作">
       <template slot-scope="scope">
         <el-button
@@ -65,14 +57,7 @@
     <span>信息</span>
        <el-input v-model="tableDataUpdata.name" placeholder="姓名" class="input"></el-input>
        <el-input v-model="tableDataUpdata.phone" placeholder="班级" class="input"></el-input>
-          <el-select v-model="tableDataUpdata.value" placeholder="请选择">
-            <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-            </el-option>
-        </el-select>
+       <el-input v-model="tableDataUpdata.banji" placeholder="所属班级" class="input"></el-input>
     <span slot="footer" class="dialog-footer">
       <el-button @click="dialogVisible = false">取 消</el-button>
       <el-button type="primary" @click="handleClose">确 定</el-button>
@@ -82,30 +67,9 @@
 </template>
 
 <script>
-// import Tanjia from '../../components/common/tianjiabuttone.vue'
-// import dayjs from 'dayjs'
   export default {
-    // components: {
-    //   Tanjia
-    // },
     data() {
       return {
-        options: [{
-          value: 1,
-          label: '数学'
-        }, {
-          value: 2,
-          label: '外语'
-        }, {
-          value: 3,
-          label: '语文'
-        }, {
-          value: 4,
-          label: '物理'
-        }, {
-          value: 5,
-          label: '化学'
-        }],
         value: '',
         tableData: '',
         tableDataUpdata: '',
@@ -121,18 +85,8 @@
       }
     },
     methods: {
-    //   handleMessage(index, row) {
-    //     this.messageVisible = true
-    //     this.$axios({
-    //       url:'/yejishow/update/get',
-    //       method: 'post',
-    //       data: {id: row.id}
-    //     }).then( result => {
-    //       this.tableDataUpdata = result.data[0]
-    //     })
-    //   },
       handleEdit(index, row) {
-        this.dialogVisible = true
+        this.dialogVisible = true;
         this.$axios({
           url:'/yejishow/update/get',
           method: 'post',
@@ -162,24 +116,12 @@
           this.handleHuoqu()
         })
       },
-      hanleadd() {
-          if(this.postdata.inputname && this.postdata.inputphone && this.postdata.inputvalue) {
-        this.$axios({
-          url:'/yejishow/add',
-          method: 'post',
-          data: this.postdata
-        }).then( result => {
-          this.$message(result)
-          this.handleHuoqu()
-        })
-          } else {
-              this.$message('数据不能为空')
-          }
-      },
       handleHuoqu() {
+        const token = window.localStorage.getItem('user-token')
         this.$axios({
           url: '/yejishow',
           method: 'get',
+          params: {"token": token}
         }).then((result) => {
           this.tableData = result.data
           this.length = result.data.length
